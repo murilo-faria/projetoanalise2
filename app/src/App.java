d43796cf09611cde83d27df24566f6ebcb3b106c
Murilo Faria <murillofs14@gmail.com>
2025-09-23 21:34:55 -0300

    primeiro commit

---
+++ app/src/App.java
@@ -0,0 +1,40 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        veiculo onix = new veiculo();
+        onix.setPlaca("ABC1234");           
+        onix.setNome("Onix");
+        onix.setCor("Prata");
+        onix.setAno(2020);
+        onix.setModelo(2020);
+        onix.setId(1);
+
+
+
+        veiculo polo = new veiculo();
+        polo.setPlaca("XYZ5678");
+        polo.setNome("Polo");
+        polo.setCor("Preto");
+        polo.setAno(2019);
+        polo.setModelo(2019);   
+        polo.setId(2);
+
+
+
+        Fabricante chevrolet = new Fabricante();
+        chevrolet.setId(1);
+        chevrolet.setNome("Chevrolet");
+        chevrolet.setCnpj("12.345.678/0001-90");
+        chevrolet.setCidade("São Paulo");
+
+        Fabricante volkswagen = new Fabricante();
+        volkswagen.setId(2);
+        volkswagen.setNome("Volkswagen");
+        volkswagen.setCnpj("98.765.432/0001-10");
+        volkswagen.setCidade("Curitiba");
+
+    }
+
+
+    veiculo.imprimircarro();
+}
---
+++ app/src/veiculo.java
@@ -0,0 +1,62 @@
+public class veiculo {
+
+     private String placa;
+    private String nome;
+    private String cor;
+    private Integer ano;
+    private Integer modelo;
+    private integer id;
+   
+
+   
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+
+    public Integer getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(Integer modelo) {
+        this.modelo = modelo;
+    }
+
+    public integer getId() {
+        return id;
+    }
+
+    public void setId(integer id) {
+        this.id = id;
+    }
+    
+
+}
